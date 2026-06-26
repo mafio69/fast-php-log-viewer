1a3ebeb60beb4ed9afb2a72b6425169df4314240
mariusz <mariusz.franciszczak@gmail.com>
2026-06-27 00:28:47 +0200

    feat: refactor LogController - extract PathResolver, FileAccessValidator, inject LogParser via DI

--- tests/Controller/LogControllerTest.php
+++ tests/Controller/LogControllerTest.php
@@ -7,8 +7,11 @@ namespace Mariusz\LogViewer\Tests\Controller;
 use Mariusz\LogViewer\Config\ConfigManager;
 use Mariusz\LogViewer\Config\LogConfig;
 use Mariusz\LogViewer\Controller\LogController;
+use Mariusz\LogViewer\Service\FileAccessValidator;
 use Mariusz\LogViewer\Service\GlobLogFinder;
 use Mariusz\LogViewer\Service\LogFinderInterface;
+use Mariusz\LogViewer\Service\LogParser;
+use Mariusz\LogViewer\Service\PathResolver;
 use PHPUnit\Framework\TestCase;
 use Slim\Psr7\Factory\RequestFactory;
 use Slim\Psr7\Factory\ResponseFactory;
@@ -16,19 +19,38 @@ use Slim\Psr7\Factory\ResponseFactory;
 class LogControllerTest extends TestCase
 {
     private LogController $controller;
-    private LogFinderInterface $logFinder;
+    private $logConfig;
+    private $logFinder;
+    private $configManager;
+    private $pathResolver;
+    private $accessValidator;
+    private $logParser;
 
     protected function setUp(): void
     {
         $this->logFinder = $this->createMock(LogFinderInterface::class);
-        $logConfig = $this->createMock(LogConfig::class);
-        $configManager = $this->createMock(ConfigManager::class);
+        $this->logConfig = $this->createMock(LogConfig::class);
+        $this->configManager = $this->createMock(ConfigManager::class);
+        $this->pathResolver = $this->createMock(PathResolver::class);
+        $this->accessValidator = $this->createMock(FileAccessValidator::class);
+        $this->logParser = $this->createMock(LogParser::class);
 
-        $this->controller = new LogController($logConfig, $configManager, $this->logFinder);
+        $this->controller = new LogController(
+            $this->logConfig,
+            $this->configManager,
+            $this->logFinder,
+            $this->pathResolver,
+            $this->accessValidator,
+            $this->logParser,
+        );
     }
 
     public function testGetFilesWithAbsolutePath(): void
     {
+        $this->pathResolver->method('resolvePath')
+            ->with('/var/log')
+            ->willReturn('/var/log');
+
         $this->logFinder->method('findAll')
             ->with('/var/log')
             ->willReturn([
@@ -53,6 +75,10 @@ class LogControllerTest extends TestCase
         $appRoot = dirname(__DIR__, 2);
         $expectedPath = $appRoot . '/logs/';
 
+        $this->pathResolver->method('resolvePath')
+            ->with('logs/')
+            ->willReturn($expectedPath);
+
         $this->logFinder->method('findAll')
             ->with($expectedPath)
             ->willReturn([
@@ -76,6 +102,10 @@ class LogControllerTest extends TestCase
     {
         $home = $_SERVER['HOME'] ?? '/root';
         $expectedPath = $home . '/logs';
+
+        $this->pathResolver->method('resolvePath')
+            ->with('~/logs')
+            ->willReturn($expectedPath);
 
         $this->logFinder->method('findAll')
             ->with($expectedPath)
@@ -108,6 +138,16 @@ class LogControllerTest extends TestCase
         $testFile = $testDir.'/test-relative-entries.log';
         $content = "[2024-01-01 10:00:00] [INFO] [test.php:1] test message\n";
         file_put_contents($testFile, $content);
+
+        $this->accessValidator->method('isFileAllowed')
+            ->with($testFile, 'logs/')
+            ->willReturn(true);
+
+        $this->logParser->method('parseFile')
+            ->with($testFile)
+            ->willReturn([
+                ['datetime' => '2024-01-01 10:00:00', 'level' => 'INFO', 'location' => 'test.php:1', 'message' => 'test message', 'context' => []],
+            ]);
 
         try {
             $request = (new RequestFactory())->createRequest(
@@ -147,6 +187,16 @@ class LogControllerTest extends TestCase
         file_put_contents($testFile, $content);
 
         $_SERVER['HOME'] = $tmpHome;
+
+        $this->accessValidator->method('isFileAllowed')
+            ->with($testFile, '~/logs')
+            ->willReturn(true);
+
+        $this->logParser->method('parseFile')
+            ->with($testFile)
+            ->willReturn([
+                ['datetime' => '2024-01-01 10:00:00', 'level' => 'WARNING', 'location' => 'app.php:5', 'message' => 'warning message', 'context' => []],
+            ]);
 
         try {
             $request = (new RequestFactory())->createRequest(
@@ -191,7 +241,15 @@ class LogControllerTest extends TestCase
         $logConfig = $this->createMock(LogConfig::class);
         $configManager = $this->createMock(ConfigManager::class);
         $realFinder = new GlobLogFinder();
-        $controller = new LogController($logConfig, $configManager, $realFinder);
+        $resolver = $this->createMock(PathResolver::class);
+        $validator = $this->createMock(FileAccessValidator::class);
+        $parser = $this->createMock(LogParser::class);
+
+        $resolver->method('resolvePath')
+            ->with($tmpDir)
+            ->willReturn($tmpDir);
+
+        $controller = new LogController($logConfig, $configManager, $realFinder, $resolver, $validator, $parser);
 
         $request = (new RequestFactory())->createRequest('GET', '/api/files?path='.urlencode($tmpDir));
         $response = (new ResponseFactory())->createResponse();
@@ -215,7 +273,15 @@ class LogControllerTest extends TestCase
         $logConfig = $this->createMock(LogConfig::class);
         $configManager = $this->createMock(ConfigManager::class);
         $realFinder = new GlobLogFinder();
-        $controller = new LogController($logConfig, $configManager, $realFinder);
+        $resolver = $this->createMock(PathResolver::class);
+        $validator = $this->createMock(FileAccessValidator::class);
+        $parser = $this->createMock(LogParser::class);
+
+        $resolver->method('resolvePath')
+            ->with('logs/')
+            ->willReturn($logDir);
+
+        $controller = new LogController($logConfig, $configManager, $realFinder, $resolver, $validator, $parser);
 
         $request = (new RequestFactory())->createRequest('GET', '/api/files?path=logs/');
         $response = (new ResponseFactory())->createResponse();
@@ -240,7 +306,15 @@ class LogControllerTest extends TestCase
         $logConfig = $this->createMock(LogConfig::class);
         $configManager = $this->createMock(ConfigManager::class);
         $realFinder = new GlobLogFinder();
-        $controller = new LogController($logConfig, $configManager, $realFinder);
+        $resolver = $this->createMock(PathResolver::class);
+        $validator = $this->createMock(FileAccessValidator::class);
+        $parser = $this->createMock(LogParser::class);
+
+        $resolver->method('resolvePath')
+            ->with('~/logs')
+            ->willReturn($logDir);
+
+        $controller = new LogController($logConfig, $configManager, $realFinder, $resolver, $validator, $parser);
 
         $request = (new RequestFactory())->createRequest('GET', '/api/files?path=~/logs');
         $response = (new ResponseFactory())->createResponse();
@@ -260,5 +334,102 @@ class LogControllerTest extends TestCase
         } else {
             unset($_SERVER['HOME']);
         }
+    }
+
+    public function testGetEntriesBlocksAccessToForbiddenPath(): void
+    {
+        $this->accessValidator->method('isFileAllowed')
+            ->with('/etc/passwd', null)
+            ->willReturn(false);
+
+        $this->logConfig->method('getDirectories')->willReturn([
+            ['name' => 'logs', 'path' => '/var/log']
+        ]);
+
+        $request = (new RequestFactory())->createRequest('GET', '/api/entries?file=/etc/passwd');
+        $response = (new ResponseFactory())->createResponse();
+
+        $result = $this->controller->getEntries($request, $response);
+
+        $this->assertEquals(403, $result->getStatusCode());
+        $body = json_decode((string)$result->getBody(), true);
+        $this->assertEquals('access_denied', $body['error']);
+    }
+
+    public function testGetEntriesBlocksPathTraversal(): void
+    {
+        $this->accessValidator->method('isFileAllowed')
+            ->with('/var/log/../secret/file.txt', null)
+            ->willReturn(false);
+
+        $this->logConfig->method('getDirectories')->willReturn([
+            ['name' => 'logs', 'path' => '/var/log']
+        ]);
+
+        $request = (new RequestFactory())->createRequest('GET', '/api/entries?file=/var/log/../secret/file.txt');
+        $response = (new ResponseFactory())->createResponse();
+
+        $result = $this->controller->getEntries($request, $response);
+
+        $this->assertEquals(403, $result->getStatusCode());
+        $body = json_decode((string)$result->getBody(), true);
+        $this->assertEquals('access_denied', $body['error']);
+    }
+
+    public function testGetFilesHandlesExceptionFromFinder(): void
+    {
+        $this->pathResolver->method('resolvePath')
+            ->with('/var/log')
+            ->willReturn('/var/log');
+
+        $this->logFinder->method('findAll')
+            ->will($this->throwException(new \Exception('Permission denied')));
+
+        $request = (new RequestFactory())->createRequest('GET', '/api/files?path=/var/log');
+        $response = (new ResponseFactory())->createResponse();
+
+        $result = $this->controller->getFiles($request, $response);
+
+        $this->assertEquals(500, $result->getStatusCode());
+        $body = json_decode((string)$result->getBody(), true);
+        $this->assertEquals('server_error', $body['error']);
+        $this->assertEquals('Permission denied', $body['message']);
+    }
+
+    public function testGetEntriesFiltersByLevelCorrectly(): void
+    {
+        $logFile = sys_get_temp_dir() . '/test_level_filter.log';
+        file_put_contents($logFile, "[2024-01-01 10:00:00] [INFO] [test.php:1] Info message\n");
+
+        $this->accessValidator->method('isFileAllowed')
+            ->with($logFile, $this->anything())
+            ->willReturn(true);
+
+        $this->logParser->method('parseFile')
+            ->with($logFile)
+            ->willReturn([
+                ['datetime' => '2024-01-01 10:00:00', 'level' => 'INFO', 'location' => 'test.php:1', 'message' => 'Info message', 'context' => []],
+                ['datetime' => '2024-01-01 10:01:00', 'level' => 'WARNING', 'location' => 'test.php:2', 'message' => 'Warning message', 'context' => []],
+                ['datetime' => '2024-01-01 10:02:00', 'level' => 'INFO', 'location' => 'test.php:3', 'message' => 'Another info message', 'context' => []],
+                ['datetime' => '2024-01-01 10:03:00', 'level' => 'ERROR', 'location' => 'test.php:4', 'message' => 'Error message', 'context' => []],
+            ]);
+
+        $this->logConfig->method('getDirectories')->willReturn([
+            ['name' => 'temp', 'path' => sys_get_temp_dir()]
+        ]);
+
+        $request = (new RequestFactory())->createRequest('GET', '/api/entries?file=' . $logFile . '&level=WARNING');
+        $response = (new ResponseFactory())->createResponse();
+
+        $result = $this->controller->getEntries($request, $response);
+
+        $this->assertEquals(200, $result->getStatusCode());
+        $body = json_decode((string)$result->getBody(), true);
+
+        $this->assertCount(1, $body);
+        $this->assertEquals('WARNING', $body[0]['level']);
+        $this->assertEquals('Warning message', $body[0]['message']);
+
+        unlink($logFile);
     }
 }
